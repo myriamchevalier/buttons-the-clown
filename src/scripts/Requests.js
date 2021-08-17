c130992e8800f28207ed1493380b9c799fc1e9dc
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-08-17 15:37:33 -0500

    added method to fecth() to sort returned request by reservationDate

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -3,6 +3,13 @@ import { getRequests } from "./dataAccess.js";
 export const reservationRequests = () => {
     
     const requests = getRequests()   //this variable holds the entire array
+   
+    // let arrayOfSortedReservations = requests.sort((a,b) => {
+    //     return parseInt(a.reservationDate.split("-").join("")) - parseInt(b.reservationDate.split("-").join(""))
+    //     
+    
+    //^^^array.sort method compares two elements contained in the array.In this case, we are comparing reservationDate key-value pair.
+    // 
 
     const reservationRequestAsListElement = (request) => {
         return `<li>Party for ${request.childName} on ${request.reservationDate} </li>`
@@ -15,3 +22,4 @@ export const reservationRequests = () => {
 
     return html
 }    
+ 
--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,12 +1,14 @@
 const applicationState = {         
-    requests: []
+    requests: [],
+    clowns: [],
+    
 }
 
 const API = "http://localhost:8088"
 const mainContainer = document.querySelector("#container")
 
 export const fetchRequests = () => {
-    return fetch(`${API}/requests`)
+    return fetch(`${API}/requests?_sort=reservationDate`) // Adding a ? after requests let you use methods on the data you're fetching. In this case _sort by reservationDate.
         .then (res => res.json())
         .then(
             (reservationRequests) => { 
@@ -14,6 +16,7 @@ export const fetchRequests = () => {
             }
         )
 }
+
 
 export const sendRequest = (userReservationRequest) => {
     const fetchOptions = {
